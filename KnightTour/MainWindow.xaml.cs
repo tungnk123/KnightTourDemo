9bfd52baa3b643e44b82be2da949baaad54a6b43
Lê Khai Trí <lekhaitri.2003@gmail.com>
2023-12-07 20:55:15 +0700

    first ui version

--- KnightTour/MainWindow.xaml.cs
+++ KnightTour/MainWindow.xaml.cs
@@ -16,23 +16,11 @@ namespace KnightTour
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             // Xử lý khi nhấn nút Bắt đầu
-            if (int.TryParse(BoardSizeTextBox.Text, out boardSize) && int.TryParse(StartPositionTextBox.Text, out startX))
-            {
-                // Gọi hàm xử lý mã đi tuần với thông số boardSize, startX, startY
-                // Code xử lý mã đi tuần ở đây
-            }
-            else
-            {
-                MessageBox.Show("Nhập thông số không hợp lệ!");
-            }
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             // Xử lý khi nhấn nút Reset
-            ChessboardCanvas.Children.Clear();
-            BoardSizeTextBox.Text = "";
-            StartPositionTextBox.Text = "";
         }
     }
 }
